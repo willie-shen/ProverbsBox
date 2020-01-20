645684d43ffc9af25d7a88b9a75ada2f610654e5
Matthew J Fala <fala@usc.edu>
2020-01-20 12:45:47 -0800

    transformed library to class component

--- proverb-box-master/src/pages/Library.tsx
+++ proverb-box-master/src/pages/Library.tsx
@@ -33,39 +33,67 @@ type ILibraryProps = {
   proverbProvider: ProverbData
 }
 
-const Library: React.FC<ILibraryProps> = (props: ILibraryProps) => {
-  const [showPopover, setShowPopover] = useState(false);
+type ILibraryState = {
+    proverbs: Array<IProverb>,
+    searchContent: string
+}
 
-  let proverbs = props.proverbProvider.GetAllOneLiners();
-  let proverbDisplay :any = proverbs.slice(0, 30).map((prov:IProverb) =>{
-    return (<Proverb key={prov.ID} Proverb={prov}></Proverb>);
-  });
+class Library extends React.Component<ILibraryProps, ILibraryState>
+{
+    /* Member data */
+    private proverbLimit = 30;
 
-  return (
-    <IonPage>
-    <IonPopover isOpen={showPopover} onDidDismiss={e => setShowPopover(false)}>
-        <p>This is popover content</p>
-     </IonPopover>
-      <IonHeader>
+    constructor(props: ILibraryProps) {
+        super(props);
+        this.state = {
+            proverbs: this.props.proverbProvider.GetAllOneLiners(),
+            searchContent: "",
+        };
+    }
 
-          <IonToolbar>
-              <IonButtons slot={"start"}>
-                <IonButton onClick={() => setShowPopover(true)}>
-                  <IonIcon slot = "icon-only" icon = {book} />
-                </IonButton>
-              </IonButtons>
+    updateProverbs() {
+        this.setState({proverbs : this.props.proverbProvider.GetFilteredOneLiners()});
+    }
 
-              <IonTitle>Library</IonTitle>
-          </IonToolbar>
-          <IonToolbar>
-            <IonSearchbar></IonSearchbar>
-          </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        {proverbDisplay}
-      </IonContent>
-    </IonPage>
-  );
+    showPopover() {
+
+    }
+
+    setShowPopover() {
+
+    }
+
+    render() {
+        let proverbDisplay :any = this.state.proverbs.slice(0, 30).map((prov:IProverb) => {
+            return (<Proverb key={prov.ID} Proverb={prov}></Proverb>);
+        });
+
+        return (
+            <IonPage>
+                <IonPopover isOpen={false} onDidDismiss={e => ()=>{/*this.setShowPopover(false)*/}}>
+                    <p>This is popover content</p>
+                </IonPopover>
+                <IonHeader>
+
+                    <IonToolbar>
+                        <IonButtons slot={"start"}>
+                            <IonButton onClick={() => {/*this.setShowPopover(true)*/}}>
+                                <IonIcon slot = "icon-only" icon = {book} />
+                            </IonButton>
+                        </IonButtons>
+
+                        <IonTitle>Library</IonTitle>
+                    </IonToolbar>
+                    <IonToolbar>
+                        <IonSearchbar></IonSearchbar>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    {proverbDisplay}
+                </IonContent>
+            </IonPage>
+        );
+    }
 };
 
 export default Library;
