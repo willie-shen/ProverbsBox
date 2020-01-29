256e481361d46fdd02adcd56bdfd97bffaa9354f
Matthew J Fala <fala@usc.edu>
2020-01-28 21:17:14 -0800

    File added

---
+++ proverb-box-master/src/api/KJV-Loader.ts
@@ -0,0 +1,17 @@
+/*
+ * Provides assistance with general book of proverbs structure.
+ * Translation independent
+ * Code for Christ, 1/23/2020
+ */
+
+import {IVerseSignature} from "./Interfaces"
+import structure from "../indexing/ProverbsStructure.json"
+import sayingsStructure from '../indexing/Sayings.json'
+import statementStructure from '../indexing/Statements.json'
+
+
+export default class Indexer {
+    Load(TranslationDataPath: string) {
+
+    }
+}
