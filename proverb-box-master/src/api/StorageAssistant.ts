075ec816aa23f7fb626e6eaa48f57a1e31a6ffca
Willie Shen <williesh@usc.edu>
2020-01-28 20:46:17 -0800

    Test for Willie + Adding Storage Assistant

--- proverb-box-master/src/api/StorageAssistant.ts
+++ proverb-box-master/src/api/StorageAssistant.ts
@@ -1,1 +1,11 @@
 
+export function BookmarkVerse(VerseID:Number){
+
+	
+}
+
+export function isBookmarked(VerseID:Number) : boolean{
+
+
+	return false
+}
--- proverb-box-master/src/api/TestScript2.ts
+++ proverb-box-master/src/api/TestScript2.ts
@@ -1,6 +1,8 @@
 /*
  * Playground for testing API.
  * Code for Christ 1/23/2020
+
+ Willie Shen
  */
 export default function TestScript() {
     console.log("TestScript 2 Running");
