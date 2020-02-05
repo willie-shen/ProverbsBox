b1ea10f5c1916a927c6ca80b0534daba76dcebc1
Willie Shen <williesh@usc.edu>
2020-02-04 20:31:36 -0800

    adding the storage stuff

--- proverb-box-master/src/api/StorageAssistant.ts
+++ proverb-box-master/src/api/StorageAssistant.ts
@@ -1,11 +1,42 @@
+import {IProverb} from "../components/ProverbInterface";
 
-export function BookmarkVerse(VerseID:Number){
+import { Plugins } from '@capacitor/core';
 
-	
+const { Storage } = Plugins;
+
+
+export class StorageAssistant{
+
+	storedIDs:Array<Number> ;
+
+	constructor(){
+		this.storedIDs = [];
+
+	}
+
+	BookmarkVerse(VerseID:Number){
+
+		//maybe call isBookmarked to see if it exists
+			//exit out the function if it is bookmarked
+
+
+		//else 
+			//"add the ID to the list"
+		if (this.isBookmarked(VerseID)){
+			return;
+		}
+
+		//https://www.tutorialsteacher.com/typescript/typescript-array
+
+		this.storedIDs.push(VerseID)
+
+
+	}
+	isBookmarked(VerseID:Number) : boolean{
+
+
+		return this.storedIDs.indexOf(VerseID) != -1;
+	}
+
 }
 
-export function isBookmarked(VerseID:Number) : boolean{
-
-
-	return false
-}
--- proverb-box-master/src/api/TestScript2.ts
+++ proverb-box-master/src/api/TestScript2.ts
@@ -4,9 +4,21 @@
 
  Willie Shen
  */
+ import {StorageAssistant} from '../api/StorageAssistant'
+
 export default function TestScript() {
     console.log("TestScript 2 Running");
     /* Test Code Here */
+
+    var storage = new StorageAssistant()
+
+    storage.BookmarkVerse(1)
+    storage.BookmarkVerse(2)
+    storage.BookmarkVerse(2)
+    storage.BookmarkVerse(3)
+
+    console.log(storage)
+
 }
 
 
