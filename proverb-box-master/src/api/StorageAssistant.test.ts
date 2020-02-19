5412c5c2122d4fe13242abd82fe0b517a444cac3
Willie Shen <williesh@usc.edu>
2020-02-18 21:03:27 -0800

    Adding remove feature and testing it

--- proverb-box-master/src/api/StorageAssistant.test.ts
+++ proverb-box-master/src/api/StorageAssistant.test.ts
@@ -40,6 +40,32 @@ test('Data is persisted 1', ()=>{
 
 	);
 
-
-
 });
+
+test("Test Removal", ()=>{
+	var storage = new StorageAssistant();
+	storage.loadFile().then(_ =>{
+		 
+		
+
+		expect(storage.isBookmarked(5)).toBe(true);
+		expect(storage.isBookmarked(7)).toBe(true);
+		expect(storage.isBookmarked(6)).toBe(true);
+
+		storage.removeBookmark(5);
+
+		expect(storage.isBookmarked(5)).toBe(false);
+		expect(storage.isBookmarked(7)).toBe(true);
+		expect(storage.isBookmarked(6)).toBe(true);
+
+	}
+
+	);
+
+	var storage1 = new StorageAssistant();
+	storage1.loadFile().then(_=>{
+		expect(storage.isBookmarked(5)).toBe(false);
+		expect(storage.isBookmarked(7)).toBe(true);
+		expect(storage.isBookmarked(6)).toBe(true);
+	});
+})
