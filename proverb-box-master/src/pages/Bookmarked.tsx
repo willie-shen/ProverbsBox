b08769f0dddc026059e345b9f7afe354accf97b5
Willie Shen <williesh@usc.edu>
2020-03-10 20:21:10 -0700

    Bookmarked.tsx is the file where there is the notification code

--- proverb-box-master/src/pages/Bookmarked.tsx
+++ proverb-box-master/src/pages/Bookmarked.tsx
@@ -16,13 +16,29 @@ import {
     IonTitle,
     IonToolbar
 } from '@ionic/react';
+
+import { Plugins } from '@capacitor/core';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 import {funnel} from 'ionicons/icons'
 const Bookmarked: React.FC = () => {
+
   const [showPopover, setShowPopover] = useState(false);
   return (
     <IonPage>
-     <IonPopover isOpen={showPopover} onDidDismiss={e => setShowPopover(false)}>
-        <p>This is popover content</p>
+     <IonPopover isOpen={showPopover} onDidDismiss={e => {
+       setShowPopover(false)
+         Plugins.LocalNotifications.schedule({
+    notifications:[{
+      title:'title',
+      body:'text',
+      id:1,
+      schedule: { at: new Date(Date.now() + 10) }
+    }]
+  
+  });
+     }}>
+        <p>This is popover</p>
      </IonPopover>
       <IonHeader>
         <IonToolbar>
