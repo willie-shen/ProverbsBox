48b40316436e75233c6fe72c19631f2acf63769e
J Hill <chirolhi@usc.edu>
2020-09-09 23:04:52 -0700

    Heart animation when clicked and remove propagation error

--- src/components/Statement.tsx
+++ src/components/Statement.tsx
@@ -28,6 +28,7 @@ type StatementProps = {
 type StatementState = {
     holdingTimer: any, // A delay event
     touchState: string, // 'n' - none, 't' - tap, 'h' - hold
+    bubbleAnimation: boolean
 }
 
 class Statement extends React.Component<StatementProps, StatementState> {
@@ -38,7 +39,8 @@ class Statement extends React.Component<StatementProps, StatementState> {
         // init state
         this.state = {
             holdingTimer: undefined,
-            touchState: 'n'
+            touchState: 'n',
+            bubbleAnimation: false
         };
     }
 
@@ -55,14 +57,14 @@ class Statement extends React.Component<StatementProps, StatementState> {
     }
 
     componentDidUpdate(prevProps: StatementProps) {
-        
+
         // if touching
         if (this.state.touchState !== 'n') {
 
             // detect scroll
             if (this.props.scrollStamp !== prevProps.scrollStamp) {
                 console.log("Scroll Detected: ", this.props.scrollStamp);
-                
+
                 // clear timed model open
                 clearTimeout(this.state.holdingTimer);
                 this.setState({
@@ -72,6 +74,15 @@ class Statement extends React.Component<StatementProps, StatementState> {
             }
         }
     }
+
+    toggleAnimation = () => {
+        console.log("HI");
+
+        this.setState({ bubbleAnimation: !this.state.bubbleAnimation });
+        setTimeout(() => {
+          this.setState({ bubbleAnimation: !this.state.bubbleAnimation });
+        }, 500);
+    };
 
     /* config */
     tapDuration = 250;
@@ -118,7 +129,7 @@ class Statement extends React.Component<StatementProps, StatementState> {
 
     saveTapped = () => {
 
-    }      
+    }
 
     render() {
 
@@ -142,7 +153,7 @@ class Statement extends React.Component<StatementProps, StatementState> {
                 this.props.searchHighlights[this.props.searchHighlights.length-1].iEnd
                 : 0;
             cardContent = this.props.searchHighlights.reduce((encode: ICardEncoding, range: ITextRange) => {
-                
+
                 const nonhighlight = cardText.substring(encode.head, range.iStart);
                 const highlight = cardText.substring(range.iStart, range.iEnd);
 
@@ -199,7 +210,7 @@ class Statement extends React.Component<StatementProps, StatementState> {
                 head: firstHighlight
             })
             // retrieve payload
-            .payload;            
+            .payload;
         }
 
         // no highlights
@@ -208,36 +219,41 @@ class Statement extends React.Component<StatementProps, StatementState> {
         }
 
         return (
-            <span
-                className={"statement"}
-                /*onTouchStart={this.gestureStart}
-                onTouchEnd={this.gestureEnd}
-                onMouseDown={this.gestureStart}
-                onMouseUp={this.gestureEnd}    */  
-                onClick={this.holdStart}          
-            >
-                <div className={"statement-view" + ((this.state.touchState === 'h') ? " shrinking" : "")}
-                    onDrag={()=>{console.log("Dragging");}}
-                    onScroll={()=>{console.log("scrolling");}}
-                >
-                    <h3 className={"verse-content"}>
-                    {
-                        cardContent
-                    }
-                    </h3>
-                    <div className={"bar"}/>
-                    <div className={"info-bar"}>
-                        <p className={"verse-name"}>Proverbs {this.props.model.Verse.Chapter}:{this.props.model.Verse.VerseNumber}</p>
-                            <IonIcon
-                                onTouchStart={(e)=>{e.stopPropagation()}}
-                                onMouseDown={(e)=>{e.stopPropagation()}}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    this.props.heartCallback();
-                                }} className={"save-icon"} icon={this.props.model.Saved ? heartCircle : heartCircleOutline}></IonIcon>
-                    </div>
-                </div>
-            </span>
+          <div className={"statement-view" + ((this.state.touchState === 'h') ? " shrinking" : "")}
+              onDrag={()=>{console.log("Dragging");}}
+              onScroll={()=>{console.log("scrolling");}}
+          >
+              <h3 className={"verse-content"}>
+              {
+                  cardContent
+              }
+              </h3>
+
+              // This Span moved for now (previous place ment as the parent of all divs here
+              // was causing Library menu to show up)
+              <span
+                  className={"statement"}
+                  /*onTouchStart={this.gestureStart}
+                  onTouchEnd={this.gestureEnd}
+                  onMouseDown={this.gestureStart}
+                  onMouseUp={this.gestureEnd}    */
+                  onClick={this.holdStart}
+              >
+              </span>
+
+              <div className={"bar"}/>
+              <div className={"info-bar"}>
+                  <p className={"verse-name"}>Proverbs {this.props.model.Verse.Chapter}:{this.props.model.Verse.VerseNumber}</p>
+                      <IonIcon
+                          onTouchStart={(e)=>{e.stopPropagation()}}
+                          onMouseDown={(e)=>{e.stopPropagation()}}
+                          onClick={(e) => {
+                              e.preventDefault();
+                              this.props.heartCallback();
+                              this.toggleAnimation();
+                          }} className={`save-icon ${this.state.bubbleAnimation ? "bubble-animation" : ""}`} icon={this.props.model.Saved ? heartCircle : heartCircleOutline}></IonIcon>
+              </div>
+          </div>
         );
     }
 }
